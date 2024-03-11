639c9dde50f418b6074f33876478364afb8b5a1c
justindomke <domke@cs.umass.edu>
2024-03-11 13:06:03 -0400

    specify python version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 
-setup(name='pangolin',
-      packages=['pangolin'],
-     )
+setup(
+    name="pangolin",
+    python_requires=">3.11",
+    packages=["pangolin"],
+)
