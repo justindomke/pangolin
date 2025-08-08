9daa32d165ea5b12f220c4c9a0107a13200e748f
justindomke <domke@cs.umass.edu>
2025-08-08 13:07:56 -0400

    fix handler input bug

--- pangolin/inference/numpyro/handler_registry.py
+++ pangolin/inference/numpyro/handler_registry.py
@@ -1,8 +1,10 @@
 from pangolin import util
 
-numpyro_handlers = util.WriteOnceDict()
+#numpyro_handlers = util.WriteOnceDict()
+# TODO: some trouble with repeated imports causing things to get registered more than once
+numpyro_handlers = {}
 
-def register_handler(op_class):
+def register_handler(op_class):    
     def doit(handle_fun):
         numpyro_handlers[op_class] = handle_fun
     return doit
