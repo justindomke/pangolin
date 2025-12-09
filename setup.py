17233ca071afe10d3bc67f45f5f08407421684e9
justindomke <domke@cs.umass.edu>
2025-12-09 08:15:24 -0500

    remove setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="pangolin",
-    python_requires=">3.11",
-    packages=["pangolin", "pangolin.ir", "pangolin.interface", "pangolin.inference"],
-)
