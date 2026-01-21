768a55157f5d9861b24b73dc1009c4305772d9a9
justindomke <domke@cs.umass.edu>
2026-01-21 21:27:18 +0530

    partial messy field implementation

--- pangolin/interface/fields.py
+++ pangolin/interface/fields.py
@@ -61,6 +61,26 @@ from typing import Sequence
 import warnings
 
 
+# How to proceed:
+#
+# The main problem is: Given a function like
+#
+# fun = lambda i, j: solve(a[i,:,:]+1, 2*b[:,i,j])
+#
+# How do we get:
+# 1) The arrays a and b being referenced
+# 2) The indices where a and b are being indexed with i / j
+# 3) dummy_fun = lambda a, b: solve(a+1, 2*b)
+#
+# If we had those things, then it would be easy to call vmap to do the actual work
+#
+# So how do we do that?
+#
+# - Create dummy scalar variables dummy_i, dummy_j
+# - Call the function on those dummy scalar variables
+# - Do a graph search on the output of the function to find all the "roots" (variables that are indexed by the dummies)
+
+
 def find_indices(nodes: Sequence[InfixRV], indices: Sequence[InfixRV[AbstractOp]]):
     return [nodes.index(idx) if idx in nodes else None for idx in indices]
 
@@ -78,49 +98,96 @@ def extract_inputs(fun, num_inputs):
     Simple single array, one index
 
     >>> a = constant([1,2,3])
-    >>> fun = lambda i: a[i]
-    >>> arrays, dims = extract_inputs(fun, 1)
+    >>> fun = lambda i: a[i] + 2.2
+    >>> arrays, dims, replay = extract_inputs(fun, 1)
     >>> arrays
     [InfixRV(Constant([1,2,3]))]
     >>> dims
     [[0]]
+    >>> replay(constant(3.3))
+    InfixRV(Add(), InfixRV(Constant(3.3)), InfixRV(Constant(2.2)))
 
     Two indices with single array
 
     >>> a = constant([[1,2,3],[4,5,6]])
-    >>> fun = lambda i, j: a[j,i]
-    >>> arrays, dims = extract_inputs(fun, 2)
+    >>> fun = lambda i, j: a[j,i] + 2.2
+    >>> arrays, dims, replay = extract_inputs(fun, 2)
     >>> arrays == [a]
     True
     >>> dims
     [[1, 0]]
+    >>> replay(constant(3.3))
+    InfixRV(Add(), InfixRV(Constant(3.3)), InfixRV(Constant(2.2)))
 
     Single array indexed twice
 
     >>> a = constant([1,2,3])
     >>> fun = lambda i: a[i] * a[i]
-    >>> arrays, dims = extract_inputs(fun, 1)
+    >>> arrays, dims, replay = extract_inputs(fun, 1)
     >>> arrays == [a, a]
     True
     >>> dims
     [[0], [0]]
+    >>> replay(constant(3.3), constant(3.3))
+    InfixRV(Mul(), InfixRV(Constant(3.3)), InfixRV(Constant(3.3)))
 
     Different indices
 
     >>> a = constant([1,2,3])
     >>> b = constant([4,5,6])
     >>> fun = lambda i, j: a[i] * b[j]
-    >>> arrays, dims = extract_inputs(fun, 2)
+    >>> arrays, dims, replay = extract_inputs(fun, 2)
     >>> arrays == [a, b]
     True
     >>> dims
     [[0, None], [None, 0]]
+    >>> replay(constant(3.3), constant(4.4))
+    InfixRV(Mul(), InfixRV(Constant(3.3)), InfixRV(Constant(4.4)))
+
+    Slice first dim of a
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([7.7, 8.8])
+    >>> fun = lambda i: a[i,:] * b[i]
+    >>> arrays, dims, replay = extract_inputs(fun, 1)
+    >>> arrays == [a, b]
+    True
+    >>> dims == [[0], [0]]
+    True
+    >>> print(replay(constant([1.1, 2.2, 3.3]), constant(4.4)))
+    vmap(mul, [0, None], 3)([1.1 2.2 3.3], 4.4)
+
+    Slice the second dimension of ``a`` and first dimension of ``b``
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([[7.7, 8.8],[9.9, 10.10],[11.11, 12.12]])
+    >>> fun = lambda i: a[:,i] @ b[i,:]
+    >>> arrays, dims, replay = extract_inputs(fun, 1)
+    >>> arrays == [a,b]
+    True
+    >>> dims == [[1],[0]]
+    True
+    >>> print(replay(constant([1.1, 4.4]), constant([7.7, 8.8])))
+    matmul([1.1 4.4], [7.7 8.8])
+
+    Slice the second dimension of ``a`` and first dimension of ``b``, different indices
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([[7.7, 8.8],[9.9, 10.10],[11.11, 12.12]])
+    >>> fun = lambda i, j: a[:,i] @ b[j,:]
+    >>> arrays, dims, replay = extract_inputs(fun, 2)
+    >>> arrays == [a,b]
+    True
+    >>> dims == [[1, None],[None, 0]]
+    True
+    >>> print(replay(constant([1.1, 4.4]), constant([7.7, 8.8])))
+    matmul([1.1 4.4], [7.7 8.8])
     """
 
     flat_fun = lambda *args: [fun(*args)]
 
     dummy_indices = [InfixRV(AbstractOp(())) for _ in range(num_inputs)]
-    all_rvs, outputs = generated_nodes(flat_fun, *dummy_indices)
+    all_rvs, [output_rv] = generated_nodes(flat_fun, *dummy_indices)
     indexed_rvs = []
     indexed_dims = []
     for rv in all_rvs:
@@ -130,32 +197,39 @@ def extract_inputs(fun, num_inputs):
 
             where_dummies = find_indices(rv.parents[1:], dummy_indices)
 
-            if all(i is None for i in where_dummies):
-                continue
+            if any(i is not None for i in where_dummies):
+                indexed_rvs.append(rv.parents[0])
+                indexed_dims.append(where_dummies)
 
-            indexed_rvs.append(rv.parents[0])
-            indexed_dims.append(where_dummies)
-    return indexed_rvs, indexed_dims
+    def replay(*indexed_rvs_replayed):
+        # all_rvs_replayed = []
+        # n = 0
 
-    # dummy_roots
-    #     Root notes for non-vmapped graph.
-    # dummy_nodes
-    #     Rest of nodes for non-vmapped graph
-    # dummy_outputs
-    #     Output nodes for non-vmapped graph (must be in dummy_node)
-    # roots
-    #     Root notes for the desired vmapped graph
-    # roots_axes
-    #     the axes along which the roots should be vectorized
-    # axis_size
-    #     the axis size for all mapped nodes (optional unless no args vmapped)
+        rv_to_replayed = {}
+
+        n = 0
+        for rv in all_rvs:
+            if isinstance(rv.op, ir.Index):
+                where_dummies = find_indices(rv.parents[1:], dummy_indices)
+
+                if any(i is not None for i in where_dummies):
+                    rv_to_replayed[rv] = indexed_rvs_replayed[n]
+                    n += 1
+                    continue
+
+            replayed_rv = InfixRV(rv.op, *[rv_to_replayed[p] for p in rv.parents])
+            rv_to_replayed[rv] = replayed_rv
+
+        return rv_to_replayed[output_rv]
+
+    return indexed_rvs, indexed_dims, replay
 
 
 # TODO: ensure no recursive indexing
 # TODO: ensure all other dimensions are full slices
 
 
-def field1(axis_size: int, fun):
+def field1(fun, axis_size: int | None = None):
     """
     Examples
     --------
@@ -163,7 +237,7 @@ def field1(axis_size: int, fun):
     Mutiply each element by two
 
     >>> x = constant([1.1, 2.2, 3.3])
-    >>> y = field1(3, lambda i: x[i]*2)
+    >>> y = field1(lambda i: x[i]*2)
     >>> print(y)
     vmap(mul, [0, None], 3)([1.1 2.2 3.3], 2)
     >>> y.parents[0] == x
@@ -171,20 +245,47 @@ def field1(axis_size: int, fun):
     >>> y.parents[1].op == ir.Constant(2)
     True
 
-    >>> x = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
-    >>> y = constant([7.7, 8.8])
-    >>> z = field1(2, lambda i: x[i,:] * y[i])
-    >>> ir.print_upstream(z)
+    Slice the first dimension of ``a``
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([7.7, 8.8])
+    >>> c = field1(lambda i: a[i,:] * b[i])
+    >>> ir.print_upstream(c)
     shape  | statement
     ------ | ---------
     (2, 3) | a = [[1.1 2.2 3.3] [4.4 5.5 6.6]]
     (2,)   | b = [7.7 8.8]
     (2, 3) | c = vmap(vmap(mul, [0, None], 3), [0, 0], 2)(a, b)
 
+    Slice the second dimension of ``a`` and first dimension of ``b``
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([[7.7, 8.8],[9.9, 10.10],[11.11, 12.12]])
+    >>> c = field1(lambda i: a[:,i] + b[i,:])
+    >>> ir.print_upstream(c)
+    shape  | statement
+    ------ | ---------
+    (2, 3) | a = [[1.1 2.2 3.3] [4.4 5.5 6.6]]
+    (3, 2) | b = [[ 7.7  8.8 ] [ 9.9 10.1 ] [11.11 12.12]]
+    (3, 2) | c = vmap(vmap(add, [0, 0], 2), [1, 0], 3)(a, b)
+
+    Slice the second dimension of ``a`` and first dimension of ``b``
+
+    >>> a = constant([[1.1, 2.2, 3.3],[4.4, 5.5, 6.6]])
+    >>> b = constant([[7.7, 8.8],[9.9, 10.10],[11.11, 12.12]])
+    >>> c = field1(lambda i: a[:,i] @ b[i,:])
+    >>> ir.print_upstream(c)
+    shape  | statement
+    ------ | ---------
+    (2, 3) | a = [[1.1 2.2 3.3] [4.4 5.5 6.6]]
+    (3, 2) | b = [[ 7.7  8.8 ] [ 9.9 10.1 ] [11.11 12.12]]
+    (3,)   | c = vmap(matmul, [1, 0], 3)(a, b)
+
+
     For technical reasons, returning an array without further processing will cause an error
 
     >>> x = constant([1.1, 2.2, 3.3])
-    >>> y = field1(3, lambda i: x[i])
+    >>> y = field1(lambda i: x[i])
     Traceback (most recent call last):
     ...
     ValueError: Output not in generated nodes. Did you perhaps return an input array?
@@ -221,17 +322,15 @@ def field1(axis_size: int, fun):
             root = rv.parents[0]
             root_axis = rv.parents[1:].index(dummy_index)
 
-            assert root.shape[root_axis] == axis_size
+            if axis_size is None:
+                axis_size = root.shape[root_axis]
+
             if root.shape[root_axis] != axis_size:
                 raise ValueError(
                     f"var mapped over axis with shape {root.shape[root_axis]} but axis size was {axis_size}"
                 )
 
-            # new_shape, new_axis_size = ir.split_shape(root.shape, root_axis)
-            # dummy_root = InfixRV(AbstractOp(new_shape))
-            dummy_root = rv
-
-            dummy_roots.append(dummy_root)
+            dummy_roots.append(rv)
             roots.append(root)
             roots_axes.append(root_axis)
         else:
@@ -242,3 +341,63 @@ def field1(axis_size: int, fun):
 
     [out] = vmap_subgraph(dummy_roots, dummy_nodes, [dummy_output], roots, roots_axes, axis_size)
     return out
+
+
+# def field(fun, axis_sizes: ir.Shape):
+#     """
+#     Examples
+#     --------
+#     >>> A = constant([[1,2,3],[4,5,6]])
+#     >>> B = constant([7,8,9])
+#     >>> field(lambda i,j: A[i,j] * B[j], (2,3))
+#     """
+
+#     dummy_indices = [InfixRV(AbstractOp(())) for _ in axis_sizes]
+
+#     def is_root(rv):
+#         if isinstance(rv.op, ir.Index):
+#             if rv.parents[0] in dummy_indices:
+#                 raise ValueError("Dummy index itself is indexed")
+#             if any(idx in rv.parents[1:] for idx in dummy_indices):
+#                 return True
+#         return False
+
+#     flat_fun = lambda *args: [fun(*args)]
+#     all_rvs, [dummy_output] = generated_nodes(flat_fun, *dummy_indices)
+
+#     for rv in all_rvs:
+#         print(rv)
+#     print(dummy_output)
+#     ir.print_upstream(dummy_output)
+
+#     dummy_roots = []
+#     dummy_nodes = []
+#     roots = []
+#     roots_axes = []
+
+#     for rv in all_rvs:
+#         if is_root(rv):
+#             for p in rv.parents:
+#                 if p in dummy_roots:
+#                     print(f"parent {p=} found in dummy_roots")
+
+#             root = rv.parents[0]
+#             root_axes = [rv.parents[1:].index(d) if d in rv.parents[1:] else None for d in dummy_indices]
+
+#             for root_axis, axis_size in zip(root_axes, axis_sizes, strict=True):
+#                 if root_axis is not None and root.shape[root_axis] != axis_size:
+#                     raise ValueError(
+#                         f"var mapped over axis with shape {root.shape[root_axis]} but axis size was {axis_size}"
+#                     )
+
+#             dummy_roots.append(rv)
+#             roots.append(root)
+#             roots_axes.append(root_axes)
+#         else:
+#             dummy_nodes.append(rv)
+
+#     if dummy_output not in dummy_nodes:
+#         raise ValueError("Output not in generated nodes. Did you perhaps return an input array?")
+
+#     # [out] = vmap_subgraph(dummy_roots, dummy_nodes, [dummy_output], roots, roots_axes, axis_size)
+#     # return out
