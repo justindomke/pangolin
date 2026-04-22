86bfa94047a9d2581e184ec2a736c9b79a1cdd2a
justindomke <domke@cs.umass.edu>
2026-04-22 10:07:59 -0400

    cleanup

--- pangolin/interface/__init__.py
+++ pangolin/interface/__init__.py
@@ -206,15 +206,3 @@ __all__ = [
     "tforms",
     "compose_transforms",
 ]
-
-# from . import base
-# from .base import *
-# from .interface import normal
-# from .vmap import vmap
-# from .composite import composite
-# from .autoregressive import autoregressive
-# from .loops import Loop, slot
-
-
-# __all__ = ['base','vmap','composite','autoregressive','Loop','slot','OperatorRV']
-# __all__ += for_api # pyright: ignore [reportUnsupportedDunderAll]
